127b0f21e5c3de2a5002408eddba6fe177ecc0e4
Pentor Saravudecha <p.saravudecha@gmail.com>
2013-06-25 15:35:18 +0700

    Update for_loops_dict.py
    Add a list inside the dictionary

--- Week 1/for_loops_dict.py
+++ Week 1/for_loops_dict.py
@@ -1,4 +1,10 @@
-person = {'name':"Peace Saravudecha", 'favorite_color':'blue','hair':'brown'}
+person = {'name':"Peace Saravudecha", 'favorite_color':'blue','hair':'brown','interests':['cycling','running','biking']}
 
 for key in person
-  print "key is " + key + " value is " + person[key]
+  
+  if (key == 'interests'):
+    print "Interests ...."
+    for interest in person[key]:
+      print "\tinterest is " + interest
+  else:
+    print "key is " + key + " value is " + person[key]
