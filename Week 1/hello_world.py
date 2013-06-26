a789b1ff487fef458e24b152c9ece979e333ac3f
Pentor Saravudecha <p.saravudecha@gmail.com>
2013-06-26 15:54:19 +0700

    Create hello_world.py
    connect on web via bottle.py framework on 'Bottle framework: Url Handlers' lecture

---
+++ Week 1/hello_world.py
@@ -0,0 +1,14 @@
+# lecture on https://www.youtube.com/watch?feature=player_embedded&v=x5TlvbWxjL0
+
+import bottle
+
+@bottle.route('/')
+def home_page():
+  return "Hello World\n"
+  
+@bottle.route('/testpage')
+def test_page():
+  return "this is a test page"
+  
+bottle.debug(True)
+bottle.run(host='localhost',port=8080)
