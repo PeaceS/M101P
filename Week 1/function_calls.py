fc6b9c00d34436c09bf4f98f03bc4773149f0dae
Pentor Saravudecha <p.saravudecha@gmail.com>
2013-06-26 15:25:13 +0700

    Update function_calls.py

--- Week 1/function_calls.py
+++ Week 1/function_calls.py
@@ -6,7 +6,7 @@ def analyze_list(l):
   counts = {}
   for item in l:
     if item in counts:
-      counts[item] = count[item] + 1
+      counts[item] = counts[item] + 1
     else:
       counts[item] = 1
       
