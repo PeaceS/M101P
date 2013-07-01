6c8b7e6069fa917dd023746890ccefb4eca7984c
Peace Saravudecha <p.saravudecha@gmail.com>
2013-07-01 14:28:02 +0700

    Complete HW 2.3

--- Week 2/HW 2.3/userDAO.py
+++ Week 2/HW 2.3/userDAO.py
@@ -55,6 +55,9 @@ class UserDAO:
         try:
             # XXX HW 2.3 Students Work Here
             # you will need to retrieve right document from the users collection.
+            user = {'_id': username}
+            user = self.users.find_one(user)
+            print user['password']
             print "This space intentionally left blank."
         except:
             print "Unable to query database for user"
@@ -85,6 +88,7 @@ class UserDAO:
             # XXX HW 2.3 Students work here
             # You need to insert the user into the users collection.
             # Don't over think this one, it's a straight forward insert.
+            self.users.insert(user)
 
             print "This space intentionally left blank."
 
